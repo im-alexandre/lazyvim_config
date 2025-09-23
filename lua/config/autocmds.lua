e5e9fc7c136b675cdd17c7aec97a93c1bdc18f04
Alexandre Castro <im.alexandre07@gmail.com>
2025-09-23 08:39:26 -0300

    autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,25 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Auto update da config do LazyVim
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- caminho do diretório de configuração
+    local config_path = vim.fn.stdpath("config")
+    local cmd = { "git", "-C", config_path, "pull", "--ff-only" }
+    vim.fn.jobstart(cmd, {
+      stdout_buffered = true,
+      stderr_buffered = true,
+      on_stdout = function(_, data)
+        if data and #data > 0 then
+          vim.notify(table.concat(data, "\n"), vim.log.levels.INFO, { title = "LazyVim Git Pull" })
+        end
+      end,
+      on_stderr = function(_, data)
+        if data and #data > 0 then
+          vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR, { title = "LazyVim Git Pull" })
+        end
+      end,
+    })
+  end,
+})
