15b83309a7365521c8587c4728db52c63b89c979
Alexandre Castro <im.alexandre07@gmail.com>
2025-09-24 12:19:58 -0300

    '-'

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = function(_, opts)
+    -- garantir que as tabelas existem
+    opts.defaults = opts.defaults or {}
+    opts.pickers = opts.pickers or {}
+
+    -- adiciona comportamento ao find_files (sem sobrescrever o resto)
+    opts.pickers.find_files = vim.tbl_deep_extend("force", opts.pickers.find_files or {}, {
+      hidden = true, -- inclui dotfiles
+      no_ignore = true, -- inclui arquivos .gitignore
+    })
+
+    return opts
+  end,
+}
