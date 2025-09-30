26971268b72e39b8dee92f2bd157d9f8743c89e8
Alexandre Castro <im.alexandre07@gmail.com>
2025-09-30 09:30:53 -0300

    -

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,11 +1,22 @@
-return {
-  {
-    "folke/snacks.nvim",
-    opts = function(_, opts)
-      -- garante que existe a tabela
-      opts.picker = opts.picker or {}
-      -- só adiciona/ajusta o que você quer
-      opts.picker.ignored = true
-    end,
-  },
+{
+  "folke/snacks.nvim",
+  opts = function(_, opts)
+    -- merge sem sobrescrever, estendendo
+    opts.animate = vim.tbl_deep_extend("force", opts.animate or {}, { enabled = false })
+    opts.scroll = vim.tbl_deep_extend("force", opts.scroll or {}, { enabled = false })
+    opts.indent = vim.tbl_deep_extend("force", opts.indent or {}, { animate = { enabled = false } })
+    opts.statuscolumn = vim.tbl_deep_extend("force", opts.statuscolumn or {}, { enabled = false })
+
+    opts.finder = vim.tbl_deep_extend("force", opts.finder or {}, {
+      cmd = "fd",
+      args = {
+        "--type", "f",
+        "--hidden",
+        "--exclude", ".git",
+        "--ignore-file", ".gitignore",
+      },
+    })
+
+    return opts
+  end,
 }
