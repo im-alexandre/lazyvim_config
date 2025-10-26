9f616cb046fc198803904f4c53d76ed542dd44a4
Alexandre Castro <im.alexandre07@gmail.com>
2025-10-25 21:54:12 -0300

    -

---
+++ lua/config/python_env.lua
@@ -0,0 +1,17 @@
+if os.getenv("CONDA_PYTHON_EXE") then
+  PYTHON_HOST = os.getenv("CONDA_PYTHON_EXE")
+else
+  PYTHON_HOST = os.getenv("CONDA_PREFIX" .. "python.exe")
+end
+
+vim.lsp.config("jedi-language-server", {
+  cmd = { PYTHON_HOST, "-m", "jedi-language-server" },
+})
+
+vim.opt.fileformats = { "unix", "dos" }
+vim.cmd([[
+  autocmd BufWritePre * set ff=unix
+]])
+
+vim.opt.shadafile = "NONE"
+require("nvim-treesitter.parsers").jsonc.revision = nil
