55cb84ed329f727026f29908b959aecc453c2466
Alexandre Castro <im.alexandre07@gmail.com>
2025-10-31 13:44:49 -0300

    -

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.autocmds")
 require("config.lazy")
 require("config.gruvbox")
 require("config.keymaps")
-require("config.autocmds")
 require("config.terminal")
 require("config.remove_powershell")
 require("config.python_env")
