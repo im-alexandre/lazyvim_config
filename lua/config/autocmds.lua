a77166d795662e1e54e5a1a52e4846b6e6fdb5b1
Alexandre Castro <im.alexandre07@gmail.com>
2025-11-04 11:11:49 -0300

    -

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -64,3 +64,32 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 vim.api.nvim_command('command! CiQuote execute "normal ci" | :stopinsert"')
+
+local progress = require("fidget.progress")
+local handles = {}
+local group = vim.api.nvim_create_augroup("CodeCompanionFidget", {})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "CodeCompanionRequestStarted",
+  group = group,
+  callback = function(e)
+    handles[e.data.id] = progress.handle.create({
+      title = "CodeCompanion",
+      message = "Pensando",
+      lsp_client = { name = e.data.adapter.formatted_name },
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "CodeCompanionRequestFinished",
+  group = group,
+  callback = function(e)
+    local h = handles[e.data.id]
+    if h then
+      h.message = e.data.status == "success" and "Done" or "Failed"
+      h:finish()
+      handles[e.data.id] = nil
+    end
+  end,
+})
